ee0c90ed8d7953f29415d5da4005772b56fc87a4
hmnf <trilesskiyars@gmail.com>
2021-08-01 14:18:34 +0300

    Router.php

--- core/Router.php
+++ core/Router.php
@@ -10,8 +10,36 @@ class Router{
         $this->request = new Request;
     }
 
+    public function routes(string $endPoint, Callable $callbackFunc)
+    {
+        $uri = $this->request->uri();
+        if($endPoint == $uri){
+            return call_user_func($callbackFunc);
+        }
+    }
+
     public function get(string $endPoint, Callable $callbackFunc)
     {
-        echo $this->request->uri();
+        $this->routes($endPoint, $callbackFunc);
+    }
+
+    public function post(string $endPoint, Callable $callbackFunc)
+    {
+        $this->routes($endPoint, $callbackFunc);
+    }
+
+    public function put(string $endPoint, Callable $callbackFunc)
+    {
+        $this->routes($endPoint, $callbackFunc);
+    }
+
+    public function patch(string $endPoint, Callable $callbackFunc)
+    {
+        $this->routes($endPoint, $callbackFunc);
+    }
+
+    public function delete(string $endPoint, Callable $callbackFunc)
+    {
+        $this->routes($endPoint, $callbackFunc);
     }
 }
